168eca2caaf70a79e66df14aedebd4ed6e878fb8
MinamotoYuki <mhdfauzanahsani@gmail.com>
2026-06-23 04:26:58 +0800

    phase 0: setup project structure

---
+++ examples/phase0.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int main(){
+    std::cout << "cpp-neural-network from the phase 0 \n";
+}
---
+++ examples/smoke.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(){
+    std::cout << "cpp-neural-network from the phase 0 smoke\n";
+}
+
